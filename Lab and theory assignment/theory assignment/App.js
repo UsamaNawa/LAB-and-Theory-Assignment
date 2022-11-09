47abb256a7a9e546cc81d69d11325764083cf9ef
UsamaNawa <114743407+UsamaNawa@users.noreply.github.com>
2022-11-09 18:22:15 +0500

    Add files via upload

---
+++ Lab and theory assignment/theory assignment/App.js
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { View } from 'react-native';
+import Ludo from './Components/Ludo';
+
+
+export default function App() {
+  
+  return (
+  <View>
+    
+    <Ludo/>
+    </View>
+  );
+}
+
+
+
+
+
