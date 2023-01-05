1090ce57d8072a889da20ea3f8673d36d1d7db47
UsamaNawa <114743407+UsamaNawa@users.noreply.github.com>
2023-01-05 19:25:19 +0500

    Add files via upload

---
+++ Lab and theory assignment # 04/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./assignment 4/Home";
+import Locator from "./assignment 4/Locator";
+import Abc from "./assignment 4/Timing"
+
+export default function App(){
+  return(
+  <Abc/>
+  );
+}
